023177b4ec430e78a593a9057bd5c1b9b2669a10
k2works <kakimomokuri@gmail.com>
2016-04-24 18:01:08 +0900

    4.4  お手軽なストラテジ

--- strategy/report.rb
+++ strategy/report.rb
@@ -4,46 +4,35 @@ class Report
   attr_reader :title, :text
   attr_accessor :formatter
 
-  def initialize(formatter)
+  def initialize(&formatter)
     @title = '月次報告'
     @text = [ '順調','最高の調子' ]
     @formatter = formatter
   end
 
   def output_report
-    @formatter.output_report(self)
-  end
-
-end
-
-class HTMLFormatter
-  def output_report(context)
-    puts('<html>')
-    puts('  <head>')
-    puts("    <title>#{context.title}</title>")
-    puts('  </head>')
-    puts('  <body>')
-    context.text.each do |line|
-      puts("     <p>#{line}</p>" )
-    end
-    puts('  </body>')
-    puts('</html>')
-  end
-end
-
-class PlainTextFormatter
-  def output_report(context)
-    puts("***** #{context.title} *****")
-    context.text.each do |line|
-      puts(line)
-    end
+    @formatter.call(self)
   end
 end
 
 describe Report do
   # HTMLレポートが出力される
   it 'should output HTML report.' do
-    @my = Report.new(HTMLFormatter.new)
+
+    HTML_FORMATTER = lambda do |context|
+      puts('<html>')
+      puts('  <head>')
+      puts("    <title>#{context.title}</title>")
+      puts('  </head>')
+      puts('  <body>')
+      context.text.each do |line|
+        puts("     <p>#{line}</p>" )
+      end
+      puts('  </body>')
+      puts('</html>')
+    end
+
+    @my = Report.new &HTML_FORMATTER
 
     report = <<-EOS
 <html>
@@ -61,7 +50,12 @@ describe Report do
 
   # プレーンテキストのレポートが出力される
   it 'should output plain text report.' do
-    @my = Report.new(PlainTextFormatter.new)
+    @my = Report.new do |context|
+      puts("***** #{context.title} *****")
+      context.text.each do |line|
+        puts(line)
+      end
+    end
 
     report = <<-EOS
 ***** 月次報告 *****
