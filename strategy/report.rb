ee0b1ea1f59c5159f0202a657db822ad43d73ffb
k2works <kakimomokuri@gmail.com>
2016-04-24 18:04:24 +0900

    ファイル分割

--- strategy/report.rb
+++ strategy/report.rb
@@ -1,5 +1,10 @@
-require 'minitest/autorun'
-
+#
+# == レポートクラス
+#
+# Author:: k2works
+# Version:: 1.0.0
+# License:: Ruby License
+#
 class Report
   attr_reader :title, :text
   attr_accessor :formatter
@@ -10,58 +15,8 @@ class Report
     @formatter = formatter
   end
 
+  # @return [String] レポート
   def output_report
     @formatter.call(self)
-  end
-end
-
-describe Report do
-  # HTMLレポートが出力される
-  it 'should output HTML report.' do
-
-    HTML_FORMATTER = lambda do |context|
-      puts('<html>')
-      puts('  <head>')
-      puts("    <title>#{context.title}</title>")
-      puts('  </head>')
-      puts('  <body>')
-      context.text.each do |line|
-        puts("     <p>#{line}</p>" )
-      end
-      puts('  </body>')
-      puts('</html>')
-    end
-
-    @my = Report.new &HTML_FORMATTER
-
-    report = <<-EOS
-<html>
-  <head>
-    <title>月次報告</title>
-  </head>
-  <body>
-     <p>順調</p>
-     <p>最高の調子</p>
-  </body>
-</html>
-    EOS
-    proc {@my.output_report}.must_output report
-  end
-
-  # プレーンテキストのレポートが出力される
-  it 'should output plain text report.' do
-    @my = Report.new do |context|
-      puts("***** #{context.title} *****")
-      context.text.each do |line|
-        puts(line)
-      end
-    end
-
-    report = <<-EOS
-***** 月次報告 *****
-順調
-最高の調子
-    EOS
-    proc {@my.output_report}.must_output report
   end
 end
