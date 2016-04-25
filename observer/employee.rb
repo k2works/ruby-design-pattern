adf7071fd81b134ba5fc4be6d6817b5374f62d22
k2works <kakimomokuri@gmail.com>
2016-04-25 10:54:25 +0900

    5.3 オブサーバに対する責務を分離する

--- observer/employee.rb
+++ observer/employee.rb
@@ -13,28 +13,9 @@ class TaxMan
   end
 end
 
-module Subject
-  def initialize
-    @observers=[]
-  end
-
-  def add_observer(observer)
-    @observers << observer
-  end
-
-  def delete_observer(observer)
-    @observers.delete(observer)
-  end
-
-  def notify_observers
-    @observers.each do |observer|
-      observer.update(self)
-    end
-  end
-end
-
+require 'observer'
 class Employee
-  include Subject
+  include Observable
 
   attr_reader :name, :address
   attr_accessor :ttile, :salary
@@ -48,7 +29,8 @@ class Employee
 
   def salary=(new_salary)
     @salary = new_salary
-    notify_observers
+    changed
+    notify_observers(self)
   end
 end
 
