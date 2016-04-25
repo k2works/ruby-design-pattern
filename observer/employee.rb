03d49445c1fa2552cc5c792b1c522bed3ab094f4
k2works <kakimomokuri@gmail.com>
2016-04-25 10:36:49 +0900

    5.2 通知を受けるよりよい方法

--- observer/employee.rb
+++ observer/employee.rb
@@ -11,28 +11,47 @@ class Employee
   attr_reader :name
   attr_accessor :ttile, :salary
 
-  def initialize( name, title, salary, payroll )
+  def initialize( name, title, salary)
     @name = name
     @title = title
     @salary = salary
-    @payroll = payroll
+    @observers = []
   end
 
   def salary=(new_salary)
     @salary = new_salary
-    @payroll.update(self)
+    notify_observers
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
   end
 end
+
 
 describe Payroll do
   # 経理部門はFredの賃金の変更を知ることができる
   it "should know the change of fred's salary." do
+    fred = Employee.new('Fred','Crane Operator', 30000)
+
     payroll = Payroll.new
-    fred = Employee.new('Fred','Crane Operator', 30000, payroll)
+    fred.add_observer( payroll )
+
     output = <<-EOS
 Fredのために小切手を切ります！
 彼の給料はいま35000です！
     EOS
+
     proc {fred.salary = 35000}.must_output output
   end
 end
