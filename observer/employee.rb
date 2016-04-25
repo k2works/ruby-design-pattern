ffa3b53a2feed7ba63c3253ada2ff8de20537e64
k2works <kakimomokuri@gmail.com>
2016-04-25 10:23:16 +0900

    5.1 通知を受ける

--- observer/employee.rb
+++ observer/employee.rb
@@ -1,21 +1,39 @@
 require 'minitest/autorun'
 
+class Payroll
+  def update( changed_employee )
+    puts("#{changed_employee.name}のために小切手を切ります！")
+    puts("彼の給料はいま#{changed_employee.salary}です！")
+  end
+end
+
 class Employee
   attr_reader :name
   attr_accessor :ttile, :salary
 
-  def initialize( name, title, salary )
+  def initialize( name, title, salary, payroll )
     @name = name
     @title = title
     @salary = salary
+    @payroll = payroll
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    @payroll.update(self)
   end
 end
 
-describe Employee do
-  # フレッドの給料が昇給している
-  it 'should raise fred salary.' do
-    fred = Employee.new("Fred Flintstone", "Crane Operator", 30000.0)
-    fred.salary.must_equal 30000.0
+describe Payroll do
+  # 経理部門はFredの賃金の変更を知ることができる
+  it "should know the change of fred's salary." do
+    payroll = Payroll.new
+    fred = Employee.new('Fred','Crane Operator', 30000, payroll)
+    output = <<-EOS
+Fredのために小切手を切ります！
+彼の給料はいま35000です！
+    EOS
+    proc {fred.salary = 35000}.must_output output
   end
 end
 
