5da768cf726a95c9fb0c0dba6471fad847132b0d
k2works <kakimomokuri@gmail.com>
2016-04-26 09:48:24 +0900

    6.2 コンポジットの作成

--- composite/Main.rb
+++ composite/Main.rb
@@ -44,15 +44,11 @@ class MixTask < Task
   end
 end
 
-class MakeBatterTask < Task
-  def initialize
-    super('Make batter')
+class CompositeTask < Task
+  def initialize(name)
+    super(name)
     @sub_tasks = []
-    add_sub_task( AddDryIngredientsTask.new )
-    add_sub_task( AddLiquidsTask.new )
-    add_sub_task( MixTask.new )
   end
-
   def add_sub_task(task)
     @sub_tasks << task
   end
@@ -68,10 +64,80 @@ class MakeBatterTask < Task
   end
 end
 
+class MakeBatterTask < CompositeTask
+  def initialize
+    super('Make batter')
+    @sub_tasks = []
+    add_sub_task( AddDryIngredientsTask.new )
+    add_sub_task( AddLiquidsTask.new )
+    add_sub_task( MixTask.new )
+  end
+end
+
+class FillPanTask < Task
+
+  def initialize
+    super('Fill pan')
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
+class BakeTask < Task
+
+  def initialize
+    super('Bake')
+  end
+
+  def get_time_required
+    10.0
+  end
+end
+
+class FrostTask < Task
+
+  def initialize
+    super('Frost')
+  end
+
+  def get_time_required
+    5.0
+  end
+end
+
+class LickSpoonTask < Task
+
+  def initialize
+    super('Lick spoon')
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
+class MakeCakeTask < CompositeTask
+  def initialize
+    super('Make cake')
+    add_sub_task( MakeBatterTask.new )
+    add_sub_task( FillPanTask.new )
+    add_sub_task( BakeTask.new )
+    add_sub_task( FrostTask.new )
+    add_sub_task( LickSpoonTask.new )
+  end
+end
+
 describe MakeBatterTask do
   # バターを作るのに５分かかる
   it 'should take 5 minutes. ' do
     batter = MakeBatterTask.new
     batter.get_time_required.must_equal 5.0
+  end
+  # ケーキを焼くのに２２分かかる
+  it 'should take 22 minutes.' do
+    cake = MakeCakeTask.new
+    cake.get_time_required.must_equal 22.0
   end
 end
