eef9da613ba335cf3e34f92f4f7fd5e362ad89ec
k2works <kakimomokuri@gmail.com>
2016-04-30 11:22:35 +0900

    7.2 内部イテレータ

--- Iterator/Main.rb
+++ Iterator/Main.rb
@@ -19,6 +19,14 @@ class ArrayIterator
     @index += 1
     value
   end
+
+  def for_each_element(array)
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+  end
 end
 
 describe ArrayIterator do
@@ -41,5 +49,28 @@ describe ArrayIterator do
       array_out << i.next_item.chr
     end
     array_out.must_equal ['a','b','c']
+  end
+
+  #コードブロックで配列の内容を順番に出力する
+  it "should output of array's content by code block." do
+    a_in = [10, 20, 30]
+    a_out = <<-EOS
+10
+20
+30
+EOS
+    i = ArrayIterator.new(a_in)
+    proc {i.for_each_element(a_in) {|element| puts element}}.must_output a_out
+  end
+
+  #eachメソッドを使ってコードブロックで配列の内容を順番に出力する
+  it "should output of array's content by code block with each method." do
+    a_in = [10, 20, 30]
+    a_out = <<-EOS
+10
+20
+30
+    EOS
+    proc {a_in.each {|element| puts element}}.must_output a_out
   end
 end
