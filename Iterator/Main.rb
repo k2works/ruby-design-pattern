455d774e9508a15bbaaf4654ca20c20f3b9c36d2
k2works <kakimomokuri@gmail.com>
2016-04-30 13:28:45 +0900

    7.6 Iteratorパターンの実例

--- Iterator/Main.rb
+++ Iterator/Main.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'pathname'
 
 class ArrayIterator
   def initialize(array)
@@ -187,4 +188,93 @@ describe Portfolio do
     @portfolio.all? {|account| account.balance >= 10}.must_equal true
   end
 
+end
+
+describe String do
+  #有名な早口言葉から文字'p'で始まる全ての単語を検索する
+  it "should output word which start from p in famus tongue twister." do
+    out =<<-EOS
+The word is Peter
+The word is Piper
+The word is picked
+The word is peck
+The word is pickled
+The word is peppers
+    EOS
+
+    s = 'Peter Piper picked a peck of pickled peppers'
+    proc{s.scan(/[Pp]\w*/) {|word| puts("The word is #{word}")}}.must_output out
+  end
+end
+
+describe Hash do
+  # ハッシュの各キーを出力する
+  it 'should output key in hash.' do
+    out =<<-EOS
+name
+eyes
+sex
+    EOS
+    h = {'name'=>'russ', 'eyes'=>'blue','sex'=>'male'}
+    proc {h.each_key { |key| puts(key)}}.must_output out
+  end
+
+  # ハッシュの各値を出力する
+  it 'should output value in hash.' do
+    out =<<-EOS
+russ
+blue
+male
+    EOS
+    h = {'name'=>'russ', 'eyes'=>'blue','sex'=>'male'}
+    proc {h.each_value { |value| puts(value)}}.must_output out
+  end
+
+  # ハッシュの内容を出力する
+  it 'should output content of hash.' do
+    out =<<-EOS
+name russ
+eyes blue
+sex male
+    EOS
+    h = {'name'=>'russ', 'eyes'=>'blue','sex'=>'male'}
+    proc {h.each {|key,value| puts("#{key} #{value}")}}.must_output out
+  end
+end
+
+describe File do
+  # ファイルの内容を出力する(外部イテレレータ)
+  it 'should output file content.' do
+    out = ''
+    f = File.open('names.txt')
+    while not f.eof?
+      out << f.readline
+    end
+    f.close
+    out.must_equal "hoge\npyo"
+  end
+  # ファイルの内容を出力する(内部イテレレータ)
+  it 'should output file content.' do
+    out = ''
+    f = File.open('names.txt')
+    f.each {|line| out << line}
+    f.close
+    out.must_equal "hoge\npyo"
+  end
+end
+
+describe Pathname do
+  before(:each) do
+    @pn = Pathname.new('/usr/local/lib/ruby')
+  end
+  # パスのコンポートネントを走査する
+  it 'should output path components.' do
+    output =<<-EOS
+File: usr
+File: local
+File: lib
+File: ruby
+    EOS
+    proc{@pn.each_filename {|file| puts("File: #{file}")}}.must_output output
+  end
 end
