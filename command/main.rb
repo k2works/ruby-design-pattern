29b920d027a25162ce947ef7e12e641550916eb0
k2works <kakimomokuri@gmail.com>
2016-04-30 15:43:21 +0900

    8.4 記録するコマンド

--- command/main.rb
+++ command/main.rb
@@ -12,6 +12,74 @@ class SlickButton
   end
 end
 
+class Command
+  attr_reader :description
+
+  def initialize(description)
+    @description = description
+  end
+
+  def execute
+  end
+end
+
+class CreateFile < Command
+  def initialize(path, contents)
+    super("Create file: #{path}")
+    @path = path
+    @contents = contents
+  end
+
+  def execute
+    f = File.open(@path, "w")
+    f.write(@contents)
+    f.close
+  end
+end
+
+class DeleteFile < Command
+  def initialize(path)
+    super("Delete file: #{path}")
+    @path = path
+  end
+
+  def execute
+    File.delete(@path)
+  end
+end
+
+class CopyFile < Command
+  def initialize(source, target)
+    super("Copy file: #{source} to #{target}")
+    @source = source
+    @target = target
+  end
+
+  def execute
+    FileUtils.copy(@source, @target)
+  end
+end
+
+class CompositeCommand < Command
+  def initialize
+    @command = []
+  end
+
+  def add_command(cmd)
+    @command << cmd
+  end
+
+  def execute
+    @command.each {|cmd| cmd.execute}
+  end
+
+  def description
+    description = ''
+    @command.each {|cmd| description += cmd.description + "\n"}
+    description
+  end
+end
+
 describe SlickButton do
   # 保存ボタンを押す
   it 'should putout message when it pushed.' do
@@ -19,5 +87,24 @@ describe SlickButton do
       puts '保存しました'
     end
     proc {new_button.on_button_push}.must_output "保存しました\n"
+  end
+end
+
+describe Command do
+  # ファイル作成・コピー・削除コマンドを実行する
+  it 'should output description of file creating,copying and delete.' do
+    output =<<-EOS
+Create file: file1.txt
+Copy file: file1.txt to file2.txt
+Delete file: file1.txt
+    EOS
+
+    cmds = CompositeCommand.new
+
+    cmds.add_command(CreateFile.new('file1.txt', "hello world\n"))
+    cmds.add_command(CopyFile.new('file1.txt', 'file2.txt'))
+    cmds.add_command(DeleteFile.new('file1.txt'))
+
+    cmds.description.must_equal output
   end
 end
