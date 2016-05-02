d6816c7128b045a484c37d39b460884861df6c18
k2works <kakimomokuri@gmail.com>
2016-05-02 11:34:11 +0900

    9.2 ニアミス

--- adapter/main.rb
+++ adapter/main.rb
@@ -34,6 +34,57 @@ class StringIOAdapter
   end
 end
 
+class Renderer
+  def render(text_object)
+    text = text_object.text
+    size = text_object.size_inches
+    color = text_object.color
+
+    # 文字列を表示します・・・
+    puts "text:#{text}"
+    puts "size:#{size}"
+    puts "color:#{color}"
+  end
+end
+
+class TextObject
+  attr_reader :text, :size_inches, :color
+
+  def initialize(text, size_inches, color)
+    @text = text
+    @size_inches = size_inches
+    @color = color
+  end
+end
+
+class BritishTextObject
+  attr_reader :string, :size_mm, :colour
+
+  def initialize(string, size_mm, colour)
+    @string = string
+    @size_mm = size_mm
+    @colour = colour
+  end
+end
+
+class BritishTextObjectAdapter < TextObject
+  def initialize(bto)
+    @bto = bto
+  end
+
+  def text
+    return @bto.string
+  end
+
+  def size_inches
+    return @bto.size_mm / 25.4
+  end
+
+  def color
+    return @bto.colour
+  end
+end
+
 describe Encrypter do
   before(:each) do
     @encrypter = Encrypter.new('my seret key')
@@ -60,5 +111,33 @@ describe StringIOAdapter do
     writer = File.open('out.txt','w')
     encrypter.encrypt(reader,writer)
     reader.wont_equal writer
+  end
+end
+
+describe Renderer do
+  before(:each) do
+    @render = Renderer.new
+  end
+
+  # TextObjectの内容が出力される
+  it 'should output contents of TextObject.' do
+    output =<<EOS
+text:Hoge
+size:25.4
+color:Blue
+EOS
+    text_object = TextObject.new('Hoge',25.4,'Blue')
+    proc{@render.render(text_object)}.must_output output
+  end
+
+  # BritishTextObjectの内容が出力される
+  it 'should output contents of BritishTextObject.' do
+    output =<<EOS
+text:Hoge
+size:1.0
+color:Blue
+EOS
+    text_object = BritishTextObjectAdapter.new(BritishTextObject.new('Hoge',25.4,'Blue'))
+    proc{@render.render(text_object)}.must_output output
   end
 end
