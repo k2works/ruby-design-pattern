e780c2ace9695d389459f925d6de613231b5d5ef
k2works <kakimomokuri@gmail.com>
2016-05-02 13:51:12 +0900

    10.5 プロキシーの重複を取り除く

--- proxy/main.rb
+++ proxy/main.rb
@@ -42,25 +42,41 @@ class AccountProtectionProxy
     @owner_name = owner_name
   end
 
-  def deposit(amount)
+  def method_missing(name, *args)
     check_access
-    return @subject.deposit(amount)
-  end
-
-  def withdraw(amount)
-    check_access
-    return @subject.withdraw(amount)
-  end
-
-  def balance
-    check_access
-    return @subject.balance
+    @subject.send( name, *args )
   end
 
   def check_access
     if Etc.getlogin != @owner_name
       raise "Illegal access: #{Etc.getlogin} cannot access account."
     end
+  end
+end
+
+class VirtualAccountProxy
+  def initialize(&creation_block)
+    @creation_block = creation_block
+  end
+
+  def method_missing(name, *args)
+    s = subject
+    s.send( name, *args)
+  end
+
+  def subject
+    @subject || (@subject = @creation_block.call)
+  end
+end
+
+class AccountProxy
+  def initialize(real_account)
+    @subject = real_account
+  end
+
+  def method_missing(name, *args)
+    puts("Delegating #{name} message to subject.")
+    @subject.send(name, *args)
   end
 end
 
@@ -91,5 +107,33 @@ describe AccountProtectionProxy do
     account = BankAccount.new(100)
     account = AccountProtectionProxy.new(account,'Illegal User')
     proc{account.deposit(50)}.must_raise RuntimeError
+  end
+end
+
+describe VirtualAccountProxy do
+  # 銀行口座の残高は140になる
+  it 'should be 140.' do
+    account = VirtualAccountProxy.new { BankAccount.new(100)}
+    account.deposit(50)
+    account.withdraw(10)
+    account.balance.must_equal 140
+  end
+end
+
+describe AccountProxy do
+  # 銀行口座に操作を移譲する
+  it 'should delegate method to Account.' do
+    output =<<EOS
+Delegating deposit message to subject.
+Delegating withdraw message to subject.
+Delegating balance message to subject.
+account balance is now: 75
+EOS
+    ap = AccountProxy.new (BankAccount.new(100))
+    proc {
+      ap.deposit(25)
+      ap.withdraw(50)
+      puts("account balance is now: #{ap.balance}")
+    }.must_output output
   end
 end
