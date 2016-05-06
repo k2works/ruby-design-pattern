b1ca6843ec943f40801a3c6628e89d21e5357fdf
k2works <kakimomokuri@gmail.com>
2016-05-06 12:31:03 +0900

    12.2 クラス変数とクラスメソッド

--- singleton/main.rb
+++ singleton/main.rb
@@ -17,6 +17,16 @@ class ClassVariableTester
   end
 end
 
+class SomeClass
+  def self.class_level_method
+    puts('hello from the class method')
+  end
+
+  def SomeClass.class_level_method2
+    puts('hello from the class method2')
+  end
+end
+
 describe ClassVariableTester do
   # １つ目のインスタンスは同じ値を持つ
   it 'should have same value.' do
@@ -27,11 +37,24 @@ describe ClassVariableTester do
   end
 
   # ２つ目のインスタンスは異なる値を持つ
-  it 'shuld not same value.' do
+  it 'should not same value.' do
     c1 = ClassVariableTester.new
     c1.increment
     c1.increment
     c2 = ClassVariableTester.new
     proc{puts("c2: #{c2}")}.must_output "c2: class_count: 4 instance_count: 0\n"
   end
+end
+
+describe SomeClass do
+  # クラスメソッドはselfで定義できる
+  it 'should define class method start with self.' do
+    proc{SomeClass.class_level_method}.must_output "hello from the class method\n"
+  end
+
+  # クラスメソッドはクラス名で定義できる
+  it 'should define class method start with class name.' do
+    proc{SomeClass.class_level_method2}.must_output "hello from the class method2\n"
+  end
+
 end
