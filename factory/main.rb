b5aad408c9775d505e7b90ba9a120da26fd64f7d
k2works <kakimomokuri@gmail.com>
2016-05-07 09:48:28 +0900

    13.2 Template Methodパターンをもう一度

--- factory/main.rb
+++ factory/main.rb
@@ -18,37 +18,84 @@ class Duck
   end
 end
 
+class Frog
+  def initialize(name)
+    @name = name
+  end
+
+  def eat
+    puts("カエル#{@name}は食事中です。")
+  end
+
+  def speak
+    puts("カエル#{@name}はゲロゲロと鳴いています。")
+  end
+
+  def sleep
+    puts("カエル#{@name}は眠りません。一晩中ゲロゲロ鳴いています。")
+  end
+end
+
 class Pond
-  def initialize(number_ducks)
-    @ducks = []
-    number_ducks.times do |i|
-      duck = Duck.new("アヒル#{i}")
-      @ducks << duck
+  def initialize(number_animals)
+    @animals = []
+    number_animals.times do |i|
+      animal = new_animal("動物#{i}")
+      @animals << animal
     end
   end
 
   def simulate_one_day
-    @ducks.each {|duck| duck.speak}
-    @ducks.each {|duck| duck.eat}
-    @ducks.each {|duck| duck.sleep}
+    @animals.each {|animal| animal.speak}
+    @animals.each {|animal| animal.eat}
+    @animals.each {|animal| animal.sleep}
+  end
+end
+
+class DuckPond < Pond
+  def new_animal(name)
+    Duck.new(name)
+  end
+end
+
+class FrogPond < Pond
+  def new_animal(name)
+    Frog.new(name)
   end
 end
 
 describe Pond do
   # 池には３匹のアヒルがいる
-  it 'should be three duck.' do
+  it 'should be three ducks.' do
     output =<<EOS
-アヒルアヒル0がガーガー鳴いています。
-アヒルアヒル1がガーガー鳴いています。
-アヒルアヒル2がガーガー鳴いています。
-アヒルアヒル0は食事中です。
-アヒルアヒル1は食事中です。
-アヒルアヒル2は食事中です。
-アヒルアヒル0は静かに眠っています。
-アヒルアヒル1は静かに眠っています。
-アヒルアヒル2は静かに眠っています。
+アヒル動物0がガーガー鳴いています。
+アヒル動物1がガーガー鳴いています。
+アヒル動物2がガーガー鳴いています。
+アヒル動物0は食事中です。
+アヒル動物1は食事中です。
+アヒル動物2は食事中です。
+アヒル動物0は静かに眠っています。
+アヒル動物1は静かに眠っています。
+アヒル動物2は静かに眠っています。
 EOS
-    pond = Pond.new(3)
+    pond = DuckPond.new(3)
+    proc{pond.simulate_one_day}.must_output output
+  end
+
+  # 池には３匹のカエルがいる
+  it 'should be three frogs.' do
+    output =<<EOS
+カエル動物0はゲロゲロと鳴いています。
+カエル動物1はゲロゲロと鳴いています。
+カエル動物2はゲロゲロと鳴いています。
+カエル動物0は食事中です。
+カエル動物1は食事中です。
+カエル動物2は食事中です。
+カエル動物0は眠りません。一晩中ゲロゲロ鳴いています。
+カエル動物1は眠りません。一晩中ゲロゲロ鳴いています。
+カエル動物2は眠りません。一晩中ゲロゲロ鳴いています。
+EOS
+    pond = FrogPond.new(3)
     proc{pond.simulate_one_day}.must_output output
   end
 end
