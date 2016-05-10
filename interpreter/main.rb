1193dfbeb1f440a8298d4b5f8de3033a49dfde67
k2works <kakimomokuri@gmail.com>
2016-05-10 11:19:32 +0900

    15.3 ファイル検索用インタープリタ

--- interpreter/main.rb
+++ interpreter/main.rb
@@ -94,6 +94,53 @@ class And < Expression
   end
 end
 
+class Parser
+  def initialize(text)
+    @tokens = text.scan(/\(|\)|[\w\.\*]+/)
+  end
+
+  def next_token
+    @tokens.shift
+  end
+
+  def expression
+    token = next_token
+
+    if token == nil
+      return nil
+
+    elsif token == '('
+      result = expression
+      raise 'Expected )' unless next_token == ')'
+      result
+
+    elsif token == 'all'
+      return All.new
+
+    elsif token == 'writable'
+      return Writable.new
+
+    elsif token == 'bigger'
+      return Bigger.new(next_token.to_i)
+
+    elsif token == 'filename'
+      return FileName.new(next_token)
+
+    elsif token == 'not'
+      return Not.new(expression)
+
+    elsif token == 'and'
+      return And.new(expression, expression)
+
+    elsif token == 'or'
+      return Or.new(expression, expression)
+
+    else
+      raise "Unxepected token: #{token}"
+    end
+  end
+end
+
 describe All do
   # テストディレクリにファイルが存在する
   it 'should be exist file in directory.' do
@@ -170,5 +217,14 @@ describe And do
                                         FileName.new('*.txt')),
                                 Not.new(Writable.new))
     complex_expression.evaluate('./test_dir').must_be_empty
+  end
+end
+
+describe Parser do
+  # テストディレクリにファイルが存在する
+  it 'should be exist file in directory.' do
+    parser = Parser.new "and (and(bigger 10) (filename *.txt)) writable"
+    ast = parser.expression
+    ast.evaluate('./test_dir').wont_be_empty
   end
 end
